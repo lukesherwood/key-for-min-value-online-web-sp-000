461091e4fd45adefd538f57c9f9add600fa176ad
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-13 06:38:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
   if lowest_value = ""
     lowest_value = value
     lowest_key = key
-  elsif lowest_value > value
+  elsif value < lowest_value
     lowest_value = value
     lowest_key = key
   end
