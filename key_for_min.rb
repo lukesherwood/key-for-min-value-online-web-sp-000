a855d85902cead3c4501d3d7c9968026c597ff8c
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-13 06:55:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,16 @@ def key_for_min_value(name_hash)
   temp_key = ""
   temp_value = ""
   name_hash.each do |key, value|
+#add key and value if none 
     if temp_value == ""
       temp_key = key
       temp_value = value
+#evaluate if next value is smaller than previous
     elsif value < temp_value
       temp_key = key
       temp_value = value
     end
   end
+#returns key with lowest value
   temp_key
 end
