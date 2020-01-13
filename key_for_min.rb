3ddc2651b7f8f79e8c0e97c8a180d01cd9e27aea
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-13 06:39:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,15 @@ def key_for_min_value(name_hash)
  lowest_value = ""
  lowest_key = ""
  name_hash.each do |key, value|
+   #return nil for hash with no key/values
+   
    #find lowest value
   if lowest_value = ""
     lowest_value = value
-    lowest_key = key
+    puts lowest_key = key
   elsif value < lowest_value
     lowest_value = value
-    lowest_key = key
+   puts lowest_key = key
   end
  end
  #return key with lowest value
